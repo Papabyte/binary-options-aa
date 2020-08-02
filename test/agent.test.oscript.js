b636384b24c8897341330052ba51aeb320c7b008
papabyte <bcreze@protonmail.com>
2020-08-02 09:40:19 +0200

    fix AA, more tests

--- test/agent.test.oscript.js
+++ test/agent.test.oscript.js
@@ -43,8 +43,8 @@ describe('Check AA', function () {
 		this.gt_operator = '>'
 		this.gt_days_for_expiration = 10
 		const time_now = new Date()
-		this.gt_expiry_date = new Date(time_now.getTime() + this.gt_days_for_expiration * 24 * 3600 * 1000).toISOString().slice(0, -14)
-
+		this.gt_expiry_date = new Date(time_now.getTime() + 10 * 24 * 3600 * 1000)
+		this.last_expiry_date = this.gt_expiry_date;
 		const { address, unit, error } = await this.network.deployer.deployAgent(`[
 			"autonomous agent",
 			{
@@ -54,7 +54,7 @@ describe('Check AA', function () {
 							"feed_name":"${this.gt_feed_name}",
 							"comparison":"${this.gt_operator}",
 							"feed_value":"${this.gt_value}",
-							"expiry_date": "${this.gt_expiry_date}"
+							"expiry_date": "${this.gt_expiry_date.toISOString().slice(0, -14)}"
 					}
 			}
 		]`)
@@ -92,7 +92,7 @@ describe('Check AA', function () {
 		var alice_balances = await this.network.wallet.alice.getBalance()
 		expect(alice_balances[this.gt_yes_asset].stable).to.be.equal(this.gt_asset_amount_alice)
 		expect(alice_balances[this.gt_no_asset].stable).to.be.equal(this.gt_asset_amount_alice)
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Bob converts bytes to greater-than asset', async () => {
 		this.gt_asset_amount_bob = 3333304
@@ -120,7 +120,7 @@ describe('Check AA', function () {
 		const bob_balances = await this.network.wallet.bob.getBalance()
 		expect(bob_balances[this.gt_yes_asset].stable).to.be.equal(this.gt_asset_amount_bob)
 		expect(bob_balances[this.gt_no_asset].stable).to.be.equal(this.gt_asset_amount_bob)
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice tries to redeem yes without any oracle posting', async () => {
 		const { unit, error } = await this.network.wallet.alice.sendMulti({
@@ -141,7 +141,7 @@ describe('Check AA', function () {
 
 		const { response } = await this.network.getAaResponseToUnit(unit)
 		expect(response.bounced).to.be.true
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice tries to redeem no without any oracle posting', async () => {
 		const { unit, error } = await this.network.wallet.alice.sendMulti({
@@ -162,7 +162,7 @@ describe('Check AA', function () {
 		const { response } = await this.network.getAaResponseToUnit(unit)
 		expect(response.bounced).to.be.true
 		expect(response.response.error).to.be.equal('suggested outcome not confirmed')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice tries to redeem no with oracle having posted only value below threshold', async () => {
 		var datafeed = {}
@@ -204,7 +204,7 @@ describe('Check AA', function () {
 		const { response } = await this.network.getAaResponseToUnit(unit)
 		expect(response.bounced).to.be.true
 		expect(response.response.error).to.be.equal('suggested outcome not confirmed')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice redeems with yes', async () => {
 		var datafeed = {}
@@ -253,10 +253,13 @@ describe('Check AA', function () {
 
 		const { vars } = await this.network.deployer.readAAStateVars(this.gt_aa)
 		expect(vars.winner).to.be.equal('yes')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('bob tries to redeem with no after expiry', async () => {
-		var { error } = await this.network.timetravel({ shift: (this.gt_days_for_expiration + 1) + 'd' })
+		var { error } = await this.network.timetravel({ to: this.gt_expiry_date })
+		expect(error).to.be.null
+
+		var { error } = await this.network.timetravel({ shift: '1d' })
 		expect(error).to.be.null
 
 		var { unit, error } = await this.network.wallet.bob.sendMulti({
@@ -277,7 +280,7 @@ describe('Check AA', function () {
 		const { response } = await this.network.getAaResponseToUnit(unit)
 		expect(response.bounced).to.be.true
 		expect(response.response.error).to.be.equal('suggested outcome not confirmed')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice tries to redeem with random after expiry', async () => {
 		var { unit, error } = await this.network.wallet.alice.sendMulti({
@@ -298,7 +301,7 @@ describe('Check AA', function () {
 		const { response } = await this.network.getAaResponseToUnit(unit)
 		expect(response.bounced).to.be.true
 		expect(response.response.error).to.be.equal('foreign asset')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('bob tries to change winner for no after expiry', async () => {
 		const { unit, error } = await this.network.wallet.bob.triggerAaWithData({
@@ -318,7 +321,7 @@ describe('Check AA', function () {
 
 		const { vars } = await this.network.deployer.readAAStateVars(this.gt_aa)
 		expect(vars.winner).to.be.equal('yes')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Bob redeems with yes', async () => {
 		var { unit, error } = await this.network.wallet.bob.sendMulti({
@@ -346,45 +349,15 @@ describe('Check AA', function () {
 
 		const { vars } = await this.network.deployer.readAAStateVars(this.gt_aa)
 		expect(vars.winner).to.be.equal('yes')
-	}).timeout(60000)
+	}).timeout(600000)
 
-	it('Deploy AA with greater than comparaison', async () => {
-		this.gt_feed_name = 'EUR_USD'
-		this.gt_value = 1.2
-		this.gt_operator = '>'
-		this.gt_days_for_expiration = 10
-		const time_now = new Date()
-		this.gt_expiry_date = new Date(time_now.getTime() + this.gt_days_for_expiration * 24 * 3600 * 1000).toISOString().slice(0, -14)
-
-		const { address, unit, error } = await this.network.deployer.deployAgent(`[
-			"autonomous agent",
-			{
-					"base_aa": "${this.network.agent.contract_base}",
-					"params": {
-							"oracle_address": "${await this.network.wallet.oracle.getAddress()}",
-							"feed_name":"${this.gt_feed_name}",
-							"comparison":"${this.gt_operator}",
-							"feed_value":"${this.gt_value}",
-							"expiry_date": "${this.gt_expiry_date}"
-					}
-			}
-		]`)
-		expect(error).to.be.null
-		expect(unit).to.be.validUnit
-
-		await this.network.witnessUntilStable(unit)
-
-		this.gt_aa = address
-	}).timeout(10000)
 
 	it('Deploy AA with equal comparaison', async () => {
 		this.equal_feed_name = 'US-PRESIDENTIAL-2024'
 		this.equal_value = 'DUMBO'
 		this.equal_operator = '='
-		this.equal_days_for_expiration = 80
-		const time_now = new Date()
-		this.equal_expiry_date = new Date(time_now.getTime() + this.gt_days_for_expiration * 24 * 3600 * 1000).toISOString().slice(0, -14)
-
+		this.equal_expiry_date = new Date(this.last_expiry_date.getTime() + 20 * 24 * 3600 * 1000)
+		this.last_expiry_date = this.equal_expiry_date;
 		const { address, unit, error } = await this.network.deployer.deployAgent(`[
 			"autonomous agent",
 			{
@@ -394,7 +367,7 @@ describe('Check AA', function () {
 							"feed_name":"${this.equal_feed_name}",
 							"comparison":"${this.equal_operator}",
 							"feed_value":"${this.equal_value}",
-							"expiry_date": "${this.equal_expiry_date}"
+							"expiry_date": "${this.equal_expiry_date.toISOString().slice(0, -14)}"
 					}
 			}
 		]`)
@@ -432,7 +405,7 @@ describe('Check AA', function () {
 		var alice_balances = await this.network.wallet.alice.getBalance()
 		expect(alice_balances[this.equal_yes_asset].stable).to.be.equal(this.equal_asset_amount_alice)
 		expect(alice_balances[this.equal_no_asset].stable).to.be.equal(this.equal_asset_amount_alice)
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice tries to flag equal-aa yes before oracle posts', async () => {
 		const { unit, error } = await this.network.wallet.bob.triggerAaWithData({
@@ -455,7 +428,7 @@ describe('Check AA', function () {
 		expect(vars.no_asset).to.be.to.be.validUnit
 		expect(vars.yes_asset).to.be.to.be.validUnit
 		expect(vars.winner).to.be.to.be.undefined
-	}).timeout(60000)
+	}).timeout(600000)
 
 
 	it('Alice tries to flag wrong outcome', async () => {
@@ -474,7 +447,7 @@ describe('Check AA', function () {
 		expect(response.response.error).to.be.equal('wrong suggested outcome')
 		expect(response.bounced).to.be.true
 
-	}).timeout(60000)
+	}).timeout(600000)
 
 
 	it('Alice flags equal-aa yes after oracle posts', async () => {
@@ -519,10 +492,11 @@ describe('Check AA', function () {
 		expect(vars.no_asset).to.be.to.be.validUnit
 		expect(vars.yes_asset).to.be.to.be.validUnit
 		expect(vars.winner).to.be.to.be.equal('yes')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice tries to redeem equal-aa with no asset', async () => {
-		var { error } = await this.network.timetravel({ shift: (this.equal_days_for_expiration + 1) + 'd' })
+		var { error } = await this.network.timetravel({ to: this.equal_expiry_date })
+		var { error } = await this.network.timetravel({ shift: '1d' })
 
 		expect(error).to.be.null
 		var { unit, error } = await this.network.wallet.alice.sendMulti({
@@ -543,7 +517,7 @@ describe('Check AA', function () {
 		const { response } = await this.network.getAaResponseToUnit(unit)
 		expect(response.bounced).to.be.true
 		expect(response.response.error).to.be.equal('suggested outcome not confirmed')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice tries to flag equal-aa with no after oracle contradicted', async () => {
 		const datafeed = {}
@@ -587,7 +561,7 @@ describe('Check AA', function () {
 		expect(vars.no_asset).to.be.to.be.validUnit
 		expect(vars.yes_asset).to.be.to.be.validUnit
 		expect(vars.winner).to.be.to.be.equal('yes')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice redeems with yes', async () => {
 		var { unit, error } = await this.network.wallet.alice.sendMulti({
@@ -615,16 +589,14 @@ describe('Check AA', function () {
 
 		const { vars } = await this.network.deployer.readAAStateVars(this.gt_aa)
 		expect(vars.winner).to.be.equal('yes')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Deploy AA with not-equal comparaison', async () => {
 		this.ne_feed_name = 'foo'
 		this.ne_value = 'bar'
 		this.ne_operator = '!='
-		this.ne_days_for_expiration = 10
-		const time_now = new Date()
-		this.ne_expiry_date = new Date(time_now.getTime() + this.ne_days_for_expiration * 24 * 3600 * 1000).toISOString().slice(0, -14)
-
+		this.ne_expiry_date = new Date(this.last_expiry_date.getTime() + 30 * 24 * 3600 * 1000)
+		this.last_expiry_date = this.ne_expiry_date;
 		const { address, unit, error } = await this.network.deployer.deployAgent(`[
 			"autonomous agent",
 			{
@@ -634,7 +606,7 @@ describe('Check AA', function () {
 						"feed_name":"${this.ne_feed_name}",
 						"comparison":"${this.ne_operator}",
 						"feed_value":"${this.ne_value}",
-						"expiry_date": "${this.ne_expiry_date}"
+						"expiry_date": "${this.ne_expiry_date.toISOString().slice(0, -14)}"
 				}
 			}
 		]`)
@@ -672,9 +644,9 @@ describe('Check AA', function () {
 		var alice_balances = await this.network.wallet.alice.getBalance()
 		expect(alice_balances[this.ne_yes_asset].stable).to.be.equal(this.ne_asset_amount_alice)
 		expect(alice_balances[this.ne_no_asset].stable).to.be.equal(this.ne_asset_amount_alice)
-	}).timeout(60000)
+	}).timeout(600000)
 
-	it('Alice flags not-equal-aa yes without any AA posting ', async () => {
+	it('Alice tries to flag not-equal-aa yes without any AA posting ', async () => {
 		var { unit, error } = await this.network.wallet.alice.triggerAaWithData({
 			toAddress: this.ne_aa,
 			amount: 10000,
@@ -695,9 +667,9 @@ describe('Check AA', function () {
 		expect(vars.no_asset).to.be.to.be.validUnit
 		expect(vars.yes_asset).to.be.to.be.validUnit
 		expect(vars.winner).to.be.to.be.undefined
-	}).timeout(60000)
+	}).timeout(600000)
 
-	it('Alice tries flags not-equal-aa yes after oracle posts ', async () => {
+	it('Alice tries to flag not-equal-aa yes after oracle posts ', async () => {
 		const datafeed = {}
 		datafeed[this.ne_feed_name] = this.ne_value
 
@@ -739,7 +711,7 @@ describe('Check AA', function () {
 		expect(vars.no_asset).to.be.to.be.validUnit
 		expect(vars.yes_asset).to.be.to.be.validUnit
 		expect(vars.winner).to.be.to.be.undefined
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice redeems half not-equal-aa with yes', async () => {
 		var datafeed = {}
@@ -789,7 +761,7 @@ describe('Check AA', function () {
 
 		const { vars } = await this.network.deployer.readAAStateVars(this.ne_aa)
 		expect(vars.winner).to.be.equal('yes')
-	}).timeout(60000)
+	}).timeout(600000)
 
 	it('Alice redeems half not-equal-aa with yes', async () => {
 		var datafeed = {}
@@ -839,7 +811,148 @@ describe('Check AA', function () {
 
 		const { vars } = await this.network.deployer.readAAStateVars(this.ne_aa)
 		expect(vars.winner).to.be.equal('yes')
-	}).timeout(60000)
+	}).timeout(600000)
+
+
+	it('Deploy AA with less-than comparaison', async () => {
+		var { error } = await this.network.timetravel({ to: this.last_expiry_date })
+		expect(error).to.be.null
+
+		this.last_expiry_date 
+		this.lt_feed_name = 'EUR_USD'
+		this.lt_value = 1.2
+		this.lt_operator = '<'
+		this.lt_days_for_expiration = 10
+		this.lt_expiry_date = new Date(this.last_expiry_date.getTime() + this.lt_days_for_expiration * 24 * 3600 * 1000).toISOString()
+		this.last_expiry_date = this.lt_expiry_date;
+
+		var { address, unit, error } = await this.network.deployer.deployAgent(`[
+			"autonomous agent",
+			{
+					"base_aa": "${this.network.agent.contract_base}",
+					"params": {
+							"oracle_address": "${await this.network.wallet.oracle.getAddress()}",
+							"feed_name":"${this.lt_feed_name}",
+							"comparison":"${this.lt_operator}",
+							"feed_value":"${this.lt_value}",
+							"expiry_date": "${this.lt_expiry_date.slice(0, -14)}"
+					}
+			}
+		]`)
+		expect(error).to.be.null
+		expect(unit).to.be.validUnit
+
+		await this.network.witnessUntilStable(unit)
+
+		this.lt_aa = address
+	}).timeout(10000)
+
+	it('bob converts bytes to less-than asset', async () => {
+		this.lt_asset_amount_bob = 196333
+		const { unit, error } = await this.network.wallet.bob.sendBytes({
+			toAddress: this.lt_aa,
+			amount: this.lt_asset_amount_bob
+		})
+
+		expect(error).to.be.null
+		expect(unit).to.be.validUnit
+
+		const { response } = await this.network.getAaResponseToUnit(unit)
+		expect(response.response.error).to.be.undefined
+
+		await this.network.witnessUntilStable(response.response_unit)
+		const { vars } = await this.network.deployer.readAAStateVars(this.lt_aa)
+
+		expect(vars.no_asset).to.be.to.be.validUnit
+		expect(vars.yes_asset).to.be.to.be.validUnit
+
+		this.lt_yes_asset = vars.yes_asset
+		this.lt_no_asset = vars.no_asset
+
+		this.lt_asset_amount_bob -= byte_to_asset_fees
+		var bob_balances = await this.network.wallet.bob.getBalance()
+		expect(bob_balances[this.lt_yes_asset].stable).to.be.equal(this.lt_asset_amount_bob)
+		expect(bob_balances[this.lt_no_asset].stable).to.be.equal(this.lt_asset_amount_bob)
+	}).timeout(600000)
+
+
+	it('bob tries to redeem with no before expiry', async () => {
+
+		var datafeed = {}
+		datafeed[this.lt_feed_name] = (this.lt_value).toString()
+
+		var objMessage = {
+			app: 'data_feed',
+			payload_location: 'inline',
+			payload_hash: objectHash.getBase64Hash(datafeed),
+			payload: datafeed
+		}
+		var opts = {
+			paying_addresses: [await this.network.wallet.oracle.getAddress()],
+			change_address: await this.network.wallet.oracle.getAddress(),
+			messages: [objMessage]
+		}
+
+		var { unit, error } = await this.network.wallet.oracle.sendMulti(opts)
+
+		var { error } = await this.network.timetravel({ shift: (this.lt_days_for_expiration - 1) + 'd' })
+		expect(error).to.be.null
+
+		var { unit, error } = await this.network.wallet.bob.sendMulti({
+			asset: this.lt_no_asset,
+			asset_outputs: [{
+				address: this.lt_aa,
+				amount: this.lt_asset_amount_bob
+			}],
+			base_outputs: [{
+				amount: 10000,
+				address: this.lt_aa
+			}]
+		})
+
+		expect(error).to.be.null
+		expect(unit).to.be.validUnit
+
+		const { response } = await this.network.getAaResponseToUnit(unit)
+		expect(response.bounced).to.be.true
+		expect(response.response.error).to.be.equal('suggested outcome not confirmed')
+		await this.network.witnessUntilStable(response.response_unit)
+	}).timeout(600000)
+
+
+	it('bob redeems with no', async () => {
+		var { error } = await this.network.timetravel({ shift: '2d' })
+		expect(error).to.be.null
+		
+
+		var { unit, error } = await this.network.wallet.bob.sendMulti({
+			asset: this.lt_no_asset,
+			asset_outputs: [{
+				address: this.lt_aa,
+				amount: this.lt_asset_amount_bob
+			}],
+			base_outputs: [{
+				amount: 10000,
+				address: this.lt_aa
+			}]
+		})
+
+		expect(error).to.be.null
+		expect(unit).to.be.validUnit
+
+		const { response } = await this.network.getAaResponseToUnit(unit)
+		expect(response.bounced).to.be.false
+		var { unitObj } = await this.network.wallet.bob.getUnitInfo({ unit: response.response_unit })
+		expect(Utils.hasOnlyTheseExternalPayments(unitObj, [{
+			address: await this.network.wallet.bob.getAddress(),
+			amount: this.lt_asset_amount_bob
+		}])).to.be.true
+
+		const { vars } = await this.network.deployer.readAAStateVars(this.lt_aa)
+		expect(vars.winner).to.be.equal('no')
+
+	}).timeout(600000)
+
 
 	after(async () => {
 		// uncomment this line to pause test execution to get time for Obyte DAG explorer inspection
